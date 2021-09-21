89a9ddec546bd67fe8b015eb68f0ec1baa46a34f
Chris Sewell <chrisj_sewell@hotmail.com>
2021-09-21 05:20:34 +0200

    🧪 TESTS: Add basic CLI tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,10 @@
 import os
 from io import StringIO
 
+from click.testing import CliRunner
+
 from process_plot import profile_process
+from process_plot.cli import cmd_exec
 
 
 def test_profile_process():
@@ -15,3 +18,21 @@ def test_profile_process():
     assert len(output_lines) == 2
     assert output_lines[0] == "PID,DATE,TIME,ELAPSED,CPU,MEM,RSS,VSIZE,CMD"
     assert output_lines[1].startswith(str(os.getpid()))
+
+
+def test_cli_help():
+    """Test the help output"""
+    runner = CliRunner()
+    result = runner.invoke(cmd_exec, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_cli_exec(tmp_path):
+    """Test the command line interface"""
+    runner = CliRunner()
+    result = runner.invoke(
+        cmd_exec, ["--basepath", str(tmp_path), "--basename", "output", "echo hi"]
+    )
+    assert result.exit_code == 0
+    assert os.path.exists(os.path.join(str(tmp_path), "output.csv"))
+    assert os.path.exists(os.path.join(str(tmp_path), "output.png"))
